bbba270ceb95967d9abc35f044a37b2957c8569d
TheChicken14 <wissehes@gmail.com>
2020-01-23 18:34:55 +0100

    Update cmd.js

--- commands/cmd.js
+++ commands/cmd.js
@@ -1,13 +1,19 @@
 exports.run = (client, message, args) => {
-    require('child_process').exec(args.join(" "), (e, sto, ste) => {
-        if(e){
-            message.author.send(e.stack)
-        }
-        if(sto){
-            message.channel.send(`STDOut - ${sto}`);
-        }
-        if(ste){
-            message.channel.send(`STDErr - ${ste}`);
-        }
-        });
+    if(message.user.id == "354289971361742848")
+    {
+        require('child_process').exec(args.join(" "), (e, sto, ste) => {
+            if(e){
+                message.author.send(e.stack)
+            }
+            if(sto){
+                message.channel.send(`STDOut - ${sto}`);
+            }
+            if(ste){
+                message.channel.send(`STDErr - ${ste}`);
+            }
+            });
+    } else {
+        message.channel.send(`nope`)
+    }
+
 }
