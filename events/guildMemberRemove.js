ce9fb8fa95ecd85430c0f31c79dd173095622c43
TheChicken14 <wissehes@gmail.com>
2020-01-23 18:37:22 +0100

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js')
 var Moment = require('moment');
 
 module.exports = (guild, member) => {
-    const moment = new Moment('2019-10-25T18:35:56.637Z')
+    const moment = new Moment(member.joinedAt)
     const joinedDate = moment.format('DD MMM, YYYY | h:mm a')
     const fromNow = moment.fromNow()
     guild.channels.get('669934302716231691')
     .send(new Discord.RichEmbed()
     .setTitle(`Someone left!`)
     .setDescription(`${member.user.tag} left!`)
-    .setThumbnail(member.user.avatarUrl)
+    .setThumbnail(member.user.avatarURL)
     .addField(`Joined at`, `${joinedDate}, ${fromNow}`)
     )
 }
