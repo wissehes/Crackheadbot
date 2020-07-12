2a05b40b2db8717c1b0163237b43adeea43d06dc
Wisse <wissehes@gmail.com>
2020-07-12 12:40:08 +0200

    Discord js v12 + removing things that arent needed

--- events/message.js
+++ events/message.js
@@ -2,45 +2,25 @@ module.exports = (client, message) => {
     if (message.author.bot) {
         return;
     }
-    if (message.channel.id == '647468258756132891') {
-        if (message.content.startsWith('**a6d**')) {
-            message.channel.send(`i dont care, a6d`)
-        }
-    }
 
     if (message.channel.type == 'dm' || message.channel.type == 'group') {
-        message.channel.send({ files: ['./data/no.mp3'] })
+        //message.channels.get().send(`\`${message.author.tag}\` said this:\n\n${message.content}`)
+        return message.channel.send({ files: ['./data/no.mp3'] })
     }
 
     const checkCommand = (client, message) => {
-        // Ignore all bots
-        //if (message.author.bot) return;
+        if (message.content.toLowerCase().indexOf(client.config.prefix) !== 0) return;
 
-        // Ignore messages not starting with the prefix (in config.json)
-        if (message.content.indexOf(client.config.prefix) !== 0) return;
-
-        // Our standard argument/command name definition.
         const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
         const command = args.shift().toLowerCase();
 
-        // Grab the command data from the client.commands Enmap
         const cmd = client.commands.get(command);
 
-        // If that command doesn't exist, silently exit and do nothing
         if (!cmd) return;
 
-        // Run the command
         cmd.run(client, message, args);
     }
     const checkWord = (client, message) => {
-        // Ignore all bots
-        //if (message.author.bot) return;
-
-        // Ignore messages not starting with the prefix (in config.json)
-        //if (message.content.indexOf(client.config.prefix) !== 0) return;
-
-        // Our standard argument/command name definition.
-        //const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
         const args = message.content.trim().split(/ +/g);
         const command = args.shift().toLowerCase();
 
