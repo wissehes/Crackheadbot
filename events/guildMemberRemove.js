14ad8857383125b3f868eee050a6e4f9824ef26f
Wisse <wissehes@gmail.com>
2020-09-13 11:36:11 +0200

    Modify guildMemberRemove event so it works with the settings

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,15 +1,30 @@
 const Discord = require('discord.js')
-var Moment = require('moment');
+const Moment = require('moment');
+const Guild = require("../db/models/Guild")
 
-module.exports = (guild, member) => {
-    const moment = new Moment(member.joinedAt)
-    const joinedDate = moment.format('DD MMM, YYYY | h:mm a')
-    const fromNow = moment.fromNow()
-    guild.channels.resolve('669934302716231691')
-        .send(new Discord.MessageEmbed()
-            .setTitle(`Someone left!`)
-            .setDescription(`${member.user.tag} left!`)
-            .setThumbnail(member.user.displayAvatarURL())
-            .addField(`Joined at`, `${joinedDate}, ${fromNow}`)
-        )
+module.exports = async (_client, member) => {
+    const settings = await Guild.findOne({ id: member.guild.id })
+
+    if (settings.memberLeftMessages) {
+        const channel = member.guild.channels.resolve(settings.memberLeftChannel)
+        if (channel) {
+            try {
+                const moment = new Moment(member.joinedAt)
+                const joinedDate = moment.format('DD MMM, YYYY | h:mm a')
+                const fromNow = moment.fromNow()
+
+                const embed = new Discord.MessageEmbed()
+                    .setTitle(`Someone left!`)
+                    .setDescription(`${member.user.tag} left!`)
+                    .setThumbnail(member.user.displayAvatarURL())
+                    .addField(`Joined at`, `${joinedDate}, ${fromNow}`)
+
+                channel.send(embed)
+            } catch (e) {
+                console.error(`Couldn't send member left message to ${channel.name} in ${member.guild.name}`)
+                console.error("Because:")
+                console.error(e)
+            }
+        }
+    }
 }
