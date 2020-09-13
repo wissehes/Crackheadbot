e877e7efcf398af75577e0b6d74ca5a29f603f51
Wisse <wissehes@gmail.com>
2020-09-13 21:26:07 +0200

    The snipe command looks way better now

--- commands/snipe.js
+++ commands/snipe.js
@@ -1,23 +1,29 @@
 const Snipe = require("../db/models/Snipe")
 const { MessageEmbed } = require("discord.js")
+const Moment = require('moment');
 
 exports.run = async (client, message, args, settings) => {
     const snipes = await Snipe.find({
         channelID: message.channel.id,
         type: "delete"
     })
-    if (snipes.length > 5) {
-        snipes.splice(5, snipes.length - 1)
-    }
+
     if (snipes.length) {
+        const sortedSnipes = snipes.sort((a, b) => new Date(b.date) - new Date(a.date))
+
+        if (sortedSnipes.length > 5) {
+            sortedSnipes.splice(5, sortedSnipes.length - 1)
+        }
         const embed = new MessageEmbed()
             .setTitle("Available snipes")
             .setAuthor(`#${message.channel.name}`)
             .setColor("RANDOM")
-            .setDescription(snipes.map(sni => {
-                const user = client.users.resolve(sni.userID)
-                return `**${user.tag}**: ${sni.message}`
-            }).join("\n"))
+        sortedSnipes.forEach((snipe, i) => {
+            const members = message.guild.members.resolve(snipe.userID)
+            const moment = new Moment(snipe.date)
+            const fromNow = moment.fromNow()
+            embed.addField(`\`#${i + 1}\` ${members.nickname || members.user.tag} ${fromNow}:`, snipe.message)
+        })
         message.channel.send(embed)
     } else {
         message.channel.send("There's nothing to snipe!")
