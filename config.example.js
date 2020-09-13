11ed0fee8d003194ff3c2eac018e4c8e37f26a50
Wisse <wissehes@gmail.com>
2020-09-13 11:58:14 +0200

    Updated example config

--- config.example.js
+++ config.example.js
@@ -1,18 +1,11 @@
-{
-    "token": "discord-token-here",
-    "prefix": "crackhead ",
-    "ownerID": "your discord user ID",
-    "twitterConsumer": "Twitter Consumer Key",
-    "twitterConsumerSecret": "Twitter Consumer Secret",
-    "twitterTokenKey": "Twitter Access Token Key",
-    "twitterTokenSecret": "Twitter Access Token Secret",
-    "twitterUsers": [
-      "array",
-      "of",
-      "twitter",
-      "user",
-      "id's",
-      "get them at http://gettwitterid.com/"
-    ],
-    "twitterChannelID": "Discord Channel ID for tweets"
+module.exports = {
+  token: "",
+  prefix: "crack ",
+  ownerID: "",
+  twitterConsumer: "",
+  twitterConsumerSecret: "",
+  twitterTokenKey: "",
+  twitterTokenSecret: "",
+  badWordsRegex: [],
+  mongouri: "mongodb://localhost/crackheadbot"
 }
