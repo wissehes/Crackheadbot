169f58a09eaae15e5110ded7f90840967a37e22c
Wisse <wissehes@gmail.com>
2020-09-13 12:19:18 +0200

    Make the config more clear

--- config.example.js
+++ config.example.js
@@ -1,11 +1,11 @@
 module.exports = {
-  token: "",
-  prefix: "crack ",
-  ownerID: "",
-  twitterConsumer: "",
-  twitterConsumerSecret: "",
-  twitterTokenKey: "",
-  twitterTokenSecret: "",
-  badWordsRegex: [],
-  mongouri: "mongodb://localhost/crackheadbot"
+  token: "", // Discord token
+  prefix: "crack ", // Prefix for commands
+  ownerID: "", // Your user id
+  twitterConsumer: "", // Twitter Consumer key
+  twitterConsumerSecret: "", // Twitter consumer secret
+  twitterTokenKey: "", // Twitter token key
+  twitterTokenSecret: "", // Twitter token secret
+  badWordsRegex: [], // An array of bad word regexes 
+  mongouri: "mongodb://localhost/crackheadbot" // MongoDB connection URI
 }
