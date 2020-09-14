f853144bb59a3360afc287f5c56bf7e5ca9f1226
Wisse <wissehes@gmail.com>
2020-09-14 14:34:36 +0200

    Add a presence to the bot

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,12 @@ const checkAllGuilds = require("../functions/checkAllGuilds")
 
 module.exports = (client) => {
     console.log(`Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
+    client.user.setPresence({
+        activity: {
+            name: `you being a crackhead 👀`,
+            type: "WATCHING"
+        }
+    })
     console.log("Checking all guilds in database")
     checkAllGuilds(client)
 }
