9b4d9636c51e25fcccea6083313d6838f7e94492
Wisse <wissehes@gmail.com>
2020-09-14 15:29:43 +0200

    Sometimes i think: "how can i be so stupid"

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,7 +1,7 @@
 const Snipe = require("../db/models/Snipe")
 
 module.exports = async (client, message) => {
-    if (oldMessage.author.bot) return;
+    if (message.author.bot) return;
     new Snipe({
         userID: message.author.id,
         channelID: message.channel.id,
