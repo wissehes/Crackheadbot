fbc2f3dd5da385eb9dd796493be8eda16906a1d7
Wisse <wissehes@gmail.com>
2020-09-14 20:37:17 +0200

    Add ability to remove rewards + some changes in the functions

--- commands/rewards.js
+++ commands/rewards.js
@@ -11,6 +11,11 @@ const isPermittedToGiveRole = (message) => {
     return message.guild.me.hasPermission("MANAGE_ROLES")
 }
 
+const displayRole = (message, roleID) => {
+    const role = message.guild.roles.resolve(roleID) || "**Not Found**"
+    return role
+}
+
 exports.run = async (client, message, args, settings) => {
     if (!settings.levels) {
         return message.channel.send(`👁👄👁 this command requires levels to be turned on`)
@@ -40,8 +45,9 @@ exports.run = async (client, message, args, settings) => {
         newReward.save()
             .then(_ => {
                 const embed = generateEmbed({
-                    role,
-                    level
+                    role: displayRole(message, role.id),
+                    level,
+                    type: "created"
                 })
                 message.channel.send(embed)
             })
@@ -55,7 +61,26 @@ exports.run = async (client, message, args, settings) => {
                 message.channel.send(errEmbed)
             })
     } else if (option === "remove") {
+        const reward = await askReward(message, "Choose the reward to remove")
 
+        reward.remove()
+            .then(_ => {
+                const embed = generateEmbed({
+                    role: displayRole(message, reward.roleID),
+                    level: reward.level,
+                    type: "removed"
+                })
+                message.channel.send(embed)
+            })
+            .catch(e => {
+                console.error("An error occurred while removing a reward:")
+                console.log(e)
+                const errEmbed = new MessageEmbed()
+                    .setTitle("An error occurred!")
+                    .setColor("RED")
+                    .setDescription("An error occurred while trying to remove your reward 🙄\n\n i'm sorry for this, pls try again in a few minutes x")
+                message.channel.send(errEmbed)
+            })
     }
 }
 
@@ -101,14 +126,53 @@ function askLevel(message) {
     })
 }
 
+function askReward(message, prompt) {
+    return new Promise(async (resolve) => {
+        const rewards = await Reward.find({
+            guildID: message.guild.id
+        })
+        const mappedRewards = rewards.map((reward, i) => {
+            const role = displayRole(message, reward.roleID)
+            return `#\`${i + 1}\`: **level**: ${reward.level}, **role**: ${role}`
+        })
+
+        const embed = new MessageEmbed()
+            .setTitle("Select reward")
+            .setColor("RANDOM")
+            .setDescription(`${prompt} \n\n${mappedRewards.join("\n")}`)
+            .setFooter("Type the number of the reward you want to choose.")
+
+        await message.channel.send(embed)
+
+        const filter = m => m.author.id === message.author.id &&
+            !isNaN(m.content) &&
+            Number(m.content) <= rewards.length &&
+            Number(m.content) > 0
+
+        message.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ['time'] })
+            .then(collected => {
+                const rewardNumber = Number(collected.first().content) - 1
+                const reward = rewards[rewardNumber]
+                resolve(reward)
+            })
+            .catch(_ => resolve(false));
+    })
+}
+
 function generateEmbed({
     role,
-    level
+    level,
+    type
 }) {
+    const title = type == "created" ? "🎉 New reward created!" : "🗑️ Reward removed!"
+    const description = type == "created"
+        ? "You just created a new reward with these settings:"
+        : "You just removed the following reward!"
+
     const embed = new MessageEmbed()
-        .setTitle("🎉 New reward created!")
+        .setTitle(title)
         .setColor("GREEN")
-        .setDescription("You just created a new reward with these settings:")
+        .setDescription(description)
         .addField("Minimum level", `Level ${level}`)
         .addField("Role reward", role)
     return embed
