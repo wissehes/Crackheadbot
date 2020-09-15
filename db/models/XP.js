1907e979f89baf03a475de6dc3a3b02cf87a3f22
Wisse <wissehes@gmail.com>
2020-09-15 16:02:43 +0200

    Added model function to get rank number for user

--- db/models/XP.js
+++ db/models/XP.js
@@ -27,4 +27,17 @@ XP.pre("save", function (next) {
     next()
 })
 
+XP.statics.getRank = async function (user) {
+    const allUsers = await this.find({})
+
+    const sorted = allUsers.sort((a, b) => b.xp - a.xp)
+
+    const mapped = sorted.map((u, i) => ({
+        userID: u.userID,
+        xp: u.xp,
+        rank: i + 1
+    }))
+    return mapped.find(u => u.userID == user.userID).rank
+}
+
 module.exports = mongoose.model("xp", XP);
