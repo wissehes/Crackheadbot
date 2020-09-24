69b82206b99d64ea1d22e4729c9db53d14351acd
Wisse <wissehes@gmail.com>
2020-09-24 16:33:08 +0200

    Add crackheadXP class to crackheadcommandoclient

--- classes/CrackheadCommandoClient.js
+++ classes/CrackheadCommandoClient.js
@@ -1,11 +1,13 @@
 const { CommandoClient, CommandoClientOptions } = require("discord.js-commando");
 const CrackheadRewards = require("./CrackheadRewards");
+const CrackheadXP = require("./CrackheadXP")
 
 class CrackheadCommandoClient extends CommandoClient {
     constructor(options) {
         super(options || new CommandoClientOptions())
 
         this.rewards = new CrackheadRewards()
+        this.xp = new CrackheadXP(this)
     }
 }
 
