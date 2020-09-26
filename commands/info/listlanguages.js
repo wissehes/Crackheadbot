b3108bbfefcd9cbe9e10d0d9e7d824f1923d0031
Wisse <wissehes@gmail.com>
2020-09-26 15:05:39 +0200

    Create listlanguages command

---
+++ commands/info/listlanguages.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require("discord.js");
+const { languages } = require("translate-google");
+const Command = require("../../classes/BaseCommand");
+
+module.exports = class ListlanguagesCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: "listlanguages",
+      aliases: ["listlang"],
+      group: "info",
+      memberName: "listlanguages",
+      description: "List all available languages for the translate command",
+    });
+  }
+  run(message) {
+    const filteredLanguages = Object.keys(languages).filter(
+      (l) => typeof languages[l] != "function"
+    );
+
+    const mappedLanguages = filteredLanguages
+      .map((l) => `\`${l}\`: ${languages[l]}`)
+      .join(" - ");
+
+    const embed = new MessageEmbed()
+      .setTitle("Available languages")
+      .setColor("BLUE")
+      .setDescription(mappedLanguages)
+      .setFooter(`Use translate {language} {text} to translate text`);
+
+    message.embed(embed);
+  }
+};
