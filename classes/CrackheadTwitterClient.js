f6845a99bbbe344064ab5075ff0efa8bb7e13c06
Wisse <wissehes@gmail.com>
2020-09-26 11:54:29 +0200

    Create crackheadtwitterclient class

---
+++ classes/CrackheadTwitterClient.js
@@ -0,0 +1,33 @@
+const Twit = require("twit");
+
+class CrackheadTwitterClient {
+  constructor(client, config) {
+    this.client = client;
+
+    this.twitter = new Twit({
+      consumer_key: config.twitterConsumer,
+      consumer_secret: config.twitterConsumerSecret,
+      access_token: config.twitterTokenKey,
+      access_token_secret: config.twitterTokenSecret,
+    });
+  }
+  /**
+   *
+   * @param {String} text Text to tweet
+   */
+  tweet(text) {
+    return new Promise((resolve, reject) => {
+      this.twitter
+        .post("statuses/update", { status: text })
+        .then(({ err, data, response }) => {
+          if (!err) {
+            resolve(data);
+          } else {
+            reject(err);
+          }
+        });
+    });
+  }
+}
+
+module.exports = CrackheadTwitterClient;
