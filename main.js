54bcc7f1458528c2b5f21d88372bd241a99e83cd
Wisse <wissehes@gmail.com>
2020-09-26 21:46:57 +0200

    reformat main file

--- main.js
+++ main.js
@@ -1,83 +1,90 @@
 // Import all things as usual
-const config = require("./config")
-const { SQLiteProvider, FriendlyError } = require('discord.js-commando');
+const config = require("./config");
+const { SQLiteProvider, FriendlyError } = require("discord.js-commando");
 const CrackheadCommandoClient = require("./classes/CrackheadCommandoClient");
 
-const path = require('path');
-const connectDB = require("./db/")
-const checkAllGuilds = require("./functions/checkAllGuilds")
+const path = require("path");
+const connectDB = require("./db/");
+const checkAllGuilds = require("./functions/checkAllGuilds");
 
-const sqlite = require('sqlite');
+const sqlite = require("sqlite");
 const sqlite3 = require("sqlite3");
 
 // Setup commando client
 const client = new CrackheadCommandoClient({
-    commandPrefix: config.prefix,
-    owner: config.ownerID,
-    invite: 'https://discord.gg/4s4QUbQ',
+  commandPrefix: config.prefix,
+  owner: config.ownerID,
+  invite: "https://discord.gg/4s4QUbQ",
 });
 
 // Register command groups
 client.registry
-    .registerDefaultTypes()
-    .registerGroups([
-        ["xp", "XP commands"],
-        ["fun", "Fun commands"],
-        ["info", "Info commands"],
-        ["admin", "Admin commands"],
-        ["moderation", "Moderation commands"],
-        ["owner", "Owner only commands"]
-    ])
-    .registerDefaultGroups()
-    .registerDefaultCommands({
-        unknownCommand: false
-    })
-    .registerCommandsIn(path.join(__dirname, 'commands'))
+  .registerDefaultTypes()
+  .registerGroups([
+    ["xp", "XP commands"],
+    ["fun", "Fun commands"],
+    ["info", "Info commands"],
+    ["admin", "Admin commands"],
+    ["moderation", "Moderation commands"],
+    ["owner", "Owner only commands"],
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands({
+    unknownCommand: false,
+  })
+  .registerCommandsIn(path.join(__dirname, "commands"));
 
-connectDB(config.mongouri)
+connectDB(config.mongouri);
 
-client.setProvider(
-    sqlite.open({ filename: path.join(__dirname, 'settings.sqlite3'), driver: sqlite3.Database }).then(db => new SQLiteProvider(db))
-).catch(console.error);
+client
+  .setProvider(
+    sqlite
+      .open({
+        filename: path.join(__dirname, "settings.sqlite3"),
+        driver: sqlite3.Database,
+      })
+      .then((db) => new SQLiteProvider(db))
+  )
+  .catch(console.error);
 
 // XP
-client.on("message", message => {
-    if (
-        message.author.bot ||
-        message.channel.type !== "text" ||
-        !message.guild.available
-    ) {
-        return;
-    }
+client.on("message", (message) => {
+  if (
+    message.author.bot ||
+    message.channel.type !== "text" ||
+    !message.guild.available
+  ) {
+    return;
+  }
 
-    client.xp.giveUserXP(message.member, message.guild)
-})
-
-client.on("messageDelete", message => {
-    client.snipes.saveSnipe(message, "delete")
-})
-
-client.on("messageUpdate", oldMessage => {
-    client.snipes.saveSnipe(oldMessage, "edit")
-})
-
-client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
-    client.user.setPresence({
-        activity: {
-            name: `you being a crackhead 👀 | ${config.prefix}help`,
-            type: "WATCHING"
-        }
-    })
-    console.log("Checking all guilds in database")
-    checkAllGuilds(client)
+  client.xp.giveUserXP(message.member, message.guild);
 });
 
-client.on('error', console.error);
-
-client.on('commandError', (cmd, err) => {
-    if (err instanceof FriendlyError)
-        console.error(`Error in command ${cmd.groupID}: ${cmd.memberName} ${err}`);
+client.on("messageDelete", (message) => {
+  client.snipes.saveSnipe(message, "delete");
 });
 
-client.login(config.token)
+client.on("messageUpdate", (oldMessage) => {
+  client.snipes.saveSnipe(oldMessage, "edit");
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+  client.user.setPresence({
+    activity: {
+      name: `you being a crackhead 👀 | ${config.prefix}help`,
+      type: "WATCHING",
+    },
+  });
+  console.log("Checking all guilds in database");
+  checkAllGuilds(client);
+});
+
+client.on("error", console.error);
+
+client.on("commandError", (cmd, err) => {
+  if (err instanceof FriendlyError)
+    console.error(`Error in command ${cmd.groupID}: ${cmd.memberName} ${err}`);
+});
+
+client.login(config.token);
