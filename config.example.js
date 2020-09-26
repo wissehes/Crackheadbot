8c27495183cb1a89940a97c0fe64c721abc8b3fb
Wisse <wissehes@gmail.com>
2020-09-26 22:56:30 +0200

    Add statsChannelID to Config

--- config.example.js
+++ config.example.js
@@ -1,11 +1,12 @@
 module.exports = {
   token: "", // Discord token
   prefix: "crack ", // Prefix for commands
+  statsChannelID: "", // Channel ID for join/leave messages
   ownerID: "", // Your user id
   twitterConsumer: "", // Twitter Consumer key
   twitterConsumerSecret: "", // Twitter consumer secret
   twitterTokenKey: "", // Twitter token key
   twitterTokenSecret: "", // Twitter token secret
-  badWordsRegex: [], // An array of bad word regexes 
-  mongouri: "mongodb://localhost/crackheadbot" // MongoDB connection URI
-}
+  badWordsRegex: [], // An array of bad word regexes
+  mongouri: "mongodb://localhost/crackheadbot", // MongoDB connection URI
+};
