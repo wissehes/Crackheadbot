4d4c8faa4c3e94202cff87da309389af590012f2
Wisse <wissehes@gmail.com>
2020-09-26 11:54:41 +0200

    Add crack twitter client to the commando class

--- classes/CrackheadCommandoClient.js
+++ classes/CrackheadCommandoClient.js
@@ -1,16 +1,22 @@
-const { CommandoClient, CommandoClientOptions } = require("discord.js-commando");
+const {
+  CommandoClient,
+  CommandoClientOptions,
+} = require("discord.js-commando");
 const CrackheadRewards = require("./CrackheadRewards");
 const CrackheadSnipes = require("./CrackheadSnipes");
-const CrackheadXP = require("./CrackheadXP")
+const CrackheadTwitterClient = require("./CrackheadTwitterClient");
+const CrackheadXP = require("./CrackheadXP");
+const config = require("../config");
 
 class CrackheadCommandoClient extends CommandoClient {
-    constructor(options) {
-        super(options || new CommandoClientOptions())
+  constructor(options) {
+    super(options || new CommandoClientOptions());
 
-        this.rewards = new CrackheadRewards()
-        this.xp = new CrackheadXP(this)
-        this.snipes = new CrackheadSnipes(this)
-    }
+    this.rewards = new CrackheadRewards();
+    this.xp = new CrackheadXP(this);
+    this.snipes = new CrackheadSnipes(this);
+    this.twitter = new CrackheadTwitterClient(this, config);
+  }
 }
 
-module.exports = CrackheadCommandoClient
+module.exports = CrackheadCommandoClient;
