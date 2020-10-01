3268be76fdb6de5941b23372a131afb81e108017
Wisse <wissehes@gmail.com>
2020-10-01 15:47:56 +0200

    Create components to show commands

---
+++ docs/src/.vuepress/components/Command.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>{{ command.name }}</h2>
+    <p>{{ command.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    command: Object,
+  },
+};
+</script>
---
+++ docs/src/.vuepress/components/Commands.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div v-if="!loading">
+      <Command
+        v-for="command in commands"
+        :key="command.name"
+        :command="command"
+      />
+    </div>
+    <div v-else>
+      <p>
+        <b> Loading... </b>
+      </p>
+    </div>
+    <span v-if="error" class="error">
+      An error occurred while fetching the commands list!
+    </span>
+  </div>
+</template>
+
+<script>
+import Command from "./Command";
+
+export default {
+  data: () => ({
+    loading: true,
+    error: false,
+    commands: [],
+  }),
+  components: {
+    Command,
+  },
+  mounted() {
+    fetch("https://crackheadbot.wissehes.nl/api/commands")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else throw new Error(response);
+      })
+      .then((commands) => {
+        this.loading = false;
+        this.commands = commands;
+      })
+      .catch((a) => (this.error = true));
+  },
+};
+</script>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
