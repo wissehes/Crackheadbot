0d024317ad9d331e7533bddb9eeb1ae6398189e6
Wisse <wissehes@gmail.com>
2020-10-02 08:38:09 +0200

    Show a badge for owner only commands and show the aliases

--- docs/src/.vuepress/components/Command.vue
+++ docs/src/.vuepress/components/Command.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
-    <h2>{{ command.name }}</h2>
-    <p>{{ command.description }}</p>
+    <h3>
+      {{ command.name }}
+      <Badge v-if="command.ownerOnly" text="owner only" type="warning" />
+    </h3>
+    <p>
+      {{ command.description }}
+    </p>
+    <details class="custom-block details" v-if="command.aliases.length > 0">
+      <p><b>Aliases:</b> {{ command.aliases.join(", ") }}</p>
+    </details>
+    <hr />
   </div>
 </template>
 
