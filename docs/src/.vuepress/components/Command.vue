42b5d425713b6d75d8d538cc3e11d3281fe4191b
Wisse <wissehes@gmail.com>
2020-10-02 19:04:41 +0200

    Better looking commands with details

--- docs/src/.vuepress/components/Command.vue
+++ docs/src/.vuepress/components/Command.vue
@@ -7,8 +7,21 @@
     <p>
       {{ command.description }}
     </p>
-    <details class="custom-block details" v-if="command.aliases.length > 0">
-      <p><b>Aliases:</b> {{ command.aliases.join(", ") }}</p>
+    <details class="custom-block details">
+      <p>
+        <b>Usage:</b> <code>{{ command.format }}</code>
+      </p>
+
+      <div class="examples" v-if="command.examples.length">
+        <p><b>Examples:</b></p>
+        <ul>
+          <li v-for="ex in command.examples" :key="ex">{{ ex }}</li>
+        </ul>
+      </div>
+
+      <p v-if="command.aliases.length > 0">
+        <b>Aliases:</b> {{ command.aliases.join(", ") }}
+      </p>
     </details>
     <hr />
   </div>
@@ -21,3 +34,12 @@ export default {
   },
 };
 </script>
+<style>
+/* Remove ugly margins */
+.examples ul {
+  margin-top: 0px;
+}
+.examples p {
+  margin-bottom: 0px;
+}
+</style>
