8056805d23893766a97292516b6f0a1ccd8006c2
Wisse <wissehes@gmail.com>
2020-10-03 14:40:43 +0200

    Format xp command

--- commands/xp/xp.js
+++ commands/xp/xp.js
@@ -1,50 +1,47 @@
-const Command = require("../../classes/BaseCommand")
+const Command = require("../../classes/BaseCommand");
 
-const {
-    MessageAttachment
-} = require("discord.js")
+const { MessageAttachment } = require("discord.js");
 
 module.exports = class XPCommand extends Command {
-    constructor(client) {
-        super(client, {
-            name: 'xp',
-            aliases: [
-                'levels',
-                'level',
-                'rank'
-            ],
-            group: 'xp',
-            memberName: 'xp',
-            description: 'Shows your xp and rank.',
-            guildOnly: true,
-            args: [
-                {
-                    key: 'member',
-                    prompt: 'Whose XP would you like to see?',
-                    type: 'member',
-                    default: msg => msg.member
-                },
-            ],
-        });
+  constructor(client) {
+    super(client, {
+      name: "xp",
+      aliases: ["levels", "level", "rank"],
+      group: "xp",
+      memberName: "xp",
+      description: "Shows your xp and rank.",
+      guildOnly: true,
+      args: [
+        {
+          key: "member",
+          prompt: "Whose XP would you like to see?",
+          type: "member",
+          default: (msg) => msg.member,
+        },
+      ],
+    });
+  }
+
+  async run(message, { member }) {
+    if (!member) {
+      return message.reply("they don't exist- 🥱");
     }
 
-    async run(message, { member }) {
-        if (!member) {
-            return message.reply("they don't exist- 🥱")
-        }
+    let card;
 
-        let card;
+    if (message.guild.me.hasPermission("ATTACH_FILES")) {
+      message.channel.startTyping();
 
-        if (message.guild.me.hasPermission("ATTACH_FILES")) {
-            message.channel.startTyping()
+      const image = await this.client.xp.getXPCard(
+        message.member,
+        message.guild
+      );
 
-            const image = await this.client.xp.getXPCard(message.member, message.guild)
-
-            card = new MessageAttachment(image, "rank.png")
-        } else {
-            card = await this.client.xp.getXPEmbed(message.member, message.guild)
-        }
-        message.channel.send(card)
-        message.channel.stopTyping()
+      card = new MessageAttachment(image, "rank.png");
+    } else {
+      card = await this.client.xp.getXPEmbed(message.member, message.guild);
     }
+    message.channel.send(card);
+    message.channel.stopTyping();
+  }
 };
