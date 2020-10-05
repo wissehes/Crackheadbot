cd73337b777888a0a46312f0eb91ffa9f4aeda43
Wisse <wissehes@gmail.com>
2020-10-05 12:07:03 +0200

    Fix typo

--- commands/games/soko.js
+++ commands/games/soko.js
@@ -51,7 +51,7 @@ module.exports = class SokoCommand extends Command {
           .setTitle("Stopped")
           .setDescription("Stopped this game of Soko!");
 
-        message.edit(stopEmbed);
+        msg.edit(stopEmbed);
       } else {
         const newGrid = soko.reGenerateGrid(text);
 
