0641c8697093cf76d2fb9c8b2d5f4337d8433b5d
Wisse <wissehes@gmail.com>
2020-11-09 21:55:24 +0100

    Add music perms to base command

--- classes/BaseCommand.js
+++ classes/BaseCommand.js
@@ -1,28 +1,31 @@
 const { Command } = require("discord.js-commando");
 
-const XP = require("../db/models/XP")
-const Guild = require("../db/models/Guild")
-const Reward = require("../db/models/Reward")
+const XP = require("../db/models/XP");
+const Guild = require("../db/models/Guild");
+const Reward = require("../db/models/Reward");
+const CrackheadMusicPerms = require("./music/CrackheadMusicPerms");
 
 class BaseCommand extends Command {
-    XP = XP
-    Guild = Guild
-    Reward = Reward
+  XP = XP;
+  Guild = Guild;
+  Reward = Reward;
 
-    getSettings = async (guild) => {
-        let settings = guild.settings.get("settings")
-        if (!settings) {
-            await guild.settings.set("settings", {
-                joined: Date.now(),
-                joinMessages: false,
-                leaveMessages: false,
-                joinChannel: "",
-                leaveChannel: "",
-                levels: false,
-            })
-            settings = guild.settings.get("settings")
-        }
-        return settings;
+  musicPerms = new CrackheadMusicPerms();
+
+  getSettings = async (guild) => {
+    let settings = guild.settings.get("settings");
+    if (!settings) {
+      await guild.settings.set("settings", {
+        joined: Date.now(),
+        joinMessages: false,
+        leaveMessages: false,
+        joinChannel: "",
+        leaveChannel: "",
+        levels: false,
+      });
+      settings = guild.settings.get("settings");
     }
+    return settings;
+  };
 }
-module.exports = BaseCommand
+module.exports = BaseCommand;
