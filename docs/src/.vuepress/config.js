ce938908a8ffd8657b7a602519db85a7043fae54
Wisse <wissehes@gmail.com>
2020-11-16 08:50:06 +0100

    fix i-

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -56,7 +56,7 @@ module.exports = {
         {
           title: "Introduction",
           collapsable: false,
-          children: ["", "using-vue"],
+          children: [""],
         },
       ],
       "/config/": [
