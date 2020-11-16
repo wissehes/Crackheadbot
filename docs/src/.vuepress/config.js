b3adb2f6cad5a124d83463a6098e2781217c39f5
Wisse <wissehes@gmail.com>
2020-11-16 10:11:33 +0100

    Add music page to sidebar

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -56,7 +56,7 @@ module.exports = {
         {
           title: "Introduction",
           collapsable: false,
-          children: [""],
+          children: ["", "music"],
         },
       ],
       "/config/": [
