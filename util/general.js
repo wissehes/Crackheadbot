f7b796616e7b958a3f58a21f587da10d559ca155
Wisse <wissehes@gmail.com>
2020-12-18 22:01:58 +0100

    Add arrayMove method

--- util/general.js
+++ util/general.js
@@ -57,6 +57,29 @@ class GeneralUtils {
 
     return average.toFixed(2);
   }
+
+  /**
+   * Move an item in an array
+   * @param {Array} arr
+   * @param {number} old_index
+   * @param {number} new_index
+   */
+  static arrayMove(arr, old_index, new_index) {
+    while (old_index < 0) {
+      old_index += arr.length;
+    }
+    while (new_index < 0) {
+      new_index += arr.length;
+    }
+    if (new_index >= arr.length) {
+      var k = new_index - arr.length + 1;
+      while (k--) {
+        arr.push(undefined);
+      }
+    }
+    arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+    return arr; // for testing purposes
+  }
 }
 
 module.exports = GeneralUtils;
